ebc17161f0a4b34a7973585e8f16b8fab15bd756
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-12 15:07:09 -0600

    Updated JS program and page navbars

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A JavaScript coding exercise using loops, functions, booleans, and conditional s
 https://jorysmith.github.io/FizzBuzz/
 
 # Description
-This program loops through 0 to 100, displays "Fizz" if the number is a multiple of 3, "Buzz" if the number is a multiple of 5, "FizzBuzz" if the number is a multiple of both 3 and 5, or simply the number if none of these conditions are met.
+This program loops through 1 to 100, displays "Fizz" if the number is a multiple of 3, "Buzz" if the number is a multiple of 5, "FizzBuzz" if the number is a multiple of both 3 and 5, or simply the number if none of these conditions are met.
 
 
--- js/site.js
+++ js/site.js
@@ -1,4 +1,45 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get user inputs for Fizz and Buzz
+function getUserInputs() {
+    let fizzNumber = document.getElementById("fizzNumber");
+    let buzzNumber = document.getElementById("buzzNumber");
+    let conditionalCheck = conditionalCheckOfInputs(fizzNumber, buzzNumber)
+}
+
+// Logic
+// Loop through 0 to 100 with for loop
+// See if each number is a multiple of 1) Fizz only 2) Buzz only 3) Both 4) Neither
+function conditionalCheckOfInputs(fizzNumber, buzzNumber) {
+    for (let i = 1; i <= 100; i++) {
+        
+        // Multiple of Both check
+        if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+            // Replace number with "FizzBuzz" string
+
+            i++
+            continue
+        }
+        // Multiple of Fizz only check   
+        if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+            // Replace number with "Fizz" string
+            i++
+            continue
+        }
+        // Multiple of Buzz only check  
+        if (i % fizzNumber == 0 && i % buzzNumber == 0) {
+            // Replace number with "Buzz" string
+            i++
+            continue
+        }
+         // Number met none of the above conditions, therefore, it's a multiple of neither
+         // Send number
+         i++
+         continue
+        }        
+    }
+}
+
+
+// Display tabular results in the DOM
+function displayResults() {
+
 }
